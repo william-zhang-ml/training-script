2f51c3840c551fc35b49f0a608bbabbde87e7b9d
william-zhang-ml <william.zhang.ml@gmail.com>
2023-02-02 01:09:23 -0500

    added function to compute accuracy as a performance metric

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ num_epochs: int = 1
 
 
 def train() -> None:
-    """ Train model. """
+    """Train model. """
     progbar = tqdm(range(num_epochs))
     num_batches = len(data_train)
     for curr_epoch in progbar:
@@ -23,6 +23,24 @@ def train() -> None:
             progbar.set_postfix({
                 'batch': f'{i_batch + 1}/{num_batches}'
             })
+
+
+def compute_accuracy(dataset: DataLoader, model) -> float:
+    """Compute classification accuracy, percent.
+
+    Args:
+        dataset (DataLoader): batches on which to evaluate.
+        model: classification model to evaluate.
+    
+    Returns:
+        float: classification accuracy, percent.
+    """
+    num_correct, num_seen = 0, 0
+    for images, labels in dataset:
+        pred = model(images).argmax(dim=-1)
+        num_correct += int((pred == labels).sum())
+        num_seen += len(images)
+    return 100 * num_correct / num_seen
 
 
 if __name__ == '__main__':
